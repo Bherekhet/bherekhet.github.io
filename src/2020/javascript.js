07dd001a58fdfde4e4e6818f4092814cd8b33a5f
Bereket Degefa <bereket.degefa@gmail.com>
2020-09-25 14:28:46 -0400

    moving to jquery

--- src/2020/javascript.js
+++ src/2020/javascript.js
@@ -1,8 +1,20 @@
-const http = new XMLHttpRequest();
-const url = 'https://bherekhet.github.io/cst-capstone-project-data/data/2020.json';
-http.open('GET', url);
-http.send();
+$(document).ready(function() {
+    const url = 'https://bherekhet.github.io/cst-capstone-project-data/data/2020.json';
+    // $.ajax({url: url, dataType: 'json', success: function(result) {
+    //     console.log(result);
+    // }})
 
-http.onreadystatechange=(e) => {
-    console.log(http.responseText)
-}
+
+    // traverse the students object and display corresponding data in a styled format
+    $.getJSON(url, {
+        format: 'json'
+    }).done(function(data) {
+        // console.log(data)
+        $.each(data, function(i, item) {
+            $.each(item, function(index, value) {
+                console.log(index, value)
+            })
+
+        })
+    })
+});
