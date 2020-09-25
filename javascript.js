392e1b1e4e09694f08db111a78327b94ea8f2873
Bereket Degefa <Bereket.degefa@gmail.com>
2020-09-24 22:45:23 -0400

    updated folder structure

--- javascript.js
+++
@@ -1,8 +0,0 @@
-const http = new XMLHttpRequest();
-const url = 'https://bherekhet.github.io/cst-capstone-project-data/data/2020.json';
-http.open('GET', url);
-http.send();
-
-http.onreadystatechange=(e) => {
-    console.log(http.responseText)
-}
