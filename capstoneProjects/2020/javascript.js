3df7159048f5075d51fe8cf7acec1c40a63cfcd3
Bereket Degefa <Bereket.degefa@gmail.com>
2020-10-05 15:29:03 -0400

    main functions of the website is complete

--- capstoneProjects/2020/javascript.js
+++ capstoneProjects/2020/javascript.js
@@ -1,9 +1,12 @@
 // "https://drive.google.com/uc?export=view&id=19MMZ3d2WTVit4xNKcSvpuR6M3Ou09Fwi",
+// setTimeout(function() {location.reload();}, 2000);
+//for image model when clicked => https://www.w3schools.com/css/css3_images.asp
 
+var image_not_found = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.thestahlman.com%2FCommon%2Fimages%2Fjquery%2Fgalleria%2Fimage-not-found.png&f=1&nofb=1";
 var cap_project;
 $(document).ready(function () {
-  var stu_id, f_name, l_name, avatar, title, desc, keywords, images, video, brochure, presentation, resume;
-  var wrapper, stu_details, stu_profile, pro_title, stu_project, proj_images, keys, stu_resources;
+  var stu_id, f_name, l_name, avatar, title, desc, keywords, images, video, brochure, presentation, resume, reso;
+  var wrapper, stu_details, stu_profile, pro_title, stu_project, proj_details, proj_images, keys, stu_resources;
 
   const url =
     "https://bherekhet.github.io/data/2020.json";
@@ -12,24 +15,49 @@ $(document).ready(function () {
   // traverse the students object and display corresponding data in a styled format
   $.getJSON(url, {
     format: "json",
-  }).done(function (data) {
+  }).done(function (data, success) {
     cap_project = data;
-    for (i in data) {
-      for (j in data[i]) {
+    if (success == 'success') {
+      console.log('this is a ' + success);
+      for (i in data) {
+        console.log('okay before ' + i)
         stu_id = data[i]["id"];
         f_name = data[i]["firstName"];
         l_name = data[i]["lastName"];
-        avatar = data[i]["profilePicture"];
+        data[i]['profilePicture'] == ""
+          ? avatar = image_not_found
+          : avatar = data[i]['profilePicture']
+        // avatar = data[i]["profilePicture"];
         title = data[i]["projectTitle"];
         desc = data[i]["projectDesc"];
-        keywords = data[i]["keywords"];
-        images = data[i]["images"];
-        presentation = data[i]['presentation'];
-        video = data[i]['video'];
-        brochure = data[i]['brochure'];
-        resume = data[i]['resume'];
+        keywords = (data[i]["keywords"]).split(',');
+
+        /*                         ---------------------- Test below case for images                           */
+        images = data[i]["images"] == ""
+          ? images = image_not_found
+          : images = data[i]["images"];
+
+        data[i]['presentation'] != ''
+          ? presentation = { 'link': data[i]['presentation'], 'color': '#99f3bd' }
+          : presentation = { 'link': data[i]['presentation'], 'color': '#faf3dd' }
+
+        data[i]['video'] != ''
+          ? video = { 'link': data[i]['video'], 'color': '#99f3bd' }
+          : video = { 'link': data[i]['video'], 'color': '#faf3dd' }
+
+        data[i]['brochure'] != ''
+          ? brochure = { 'link': data[i]['brochure'], 'color': '#99f3bd' }
+          : brochure = { 'link': data[i]['brochure'], 'color': '#faf3dd' }
+
+        data[i]['resume'] != ''
+          ? resume = { 'link': data[i]['resume'], 'color': '#99f3bd' }
+          : resume = { 'link': data[i]['resume'], 'color': '#faf3dd' }
+
+        createElements();
       }
-      createElements();
+
+    } else {
+      console.log('Something went wrong, data is corrupt');
     }
   });
 
@@ -40,12 +68,6 @@ $(document).ready(function () {
       id: "wrapper"
     });
 
-    //student profile picture
-    stu_profile = $('<div>', {
-      class: 'avatar'
-    }).append(`<img src=${avatar}>`);
-    wrapper.append(stu_profile);
-
     //div to hold student name
     stu_details = $("<div>", {
       id: "details"
@@ -55,22 +77,29 @@ $(document).ready(function () {
     }).text(f_name));
     stu_details.append($("<span>", {
       class: "lname"
-    }).text(l_name));
-    wrapper.append(stu_details);
+    }).text(' ' + l_name));
+
+    //student profile picture
+    stu_profile = $('<div>', {
+      class: 'student_profile'
+    })
+
+    stu_profile.append(stu_details);
+    stu_profile.append(`<img src=${avatar} alt="${f_name} ${l_name}'s image">`)
+    wrapper.append(stu_profile);
 
     //Student project div
     stu_project = $("<div>", {
-      class: "project"
+      class: "project_visuals"
     });
 
     //a div for project image, title, description and more-buttons
-    pro_title = $("<div>", {
-      class: 'title'
-    }).append($("<span>", {
-      class: "title"
-    }).text(title));
-    stu_project.append(pro_title);
-
+    // pro_title = $("<div>", {
+    //   class: 'title'
+    // }).append($("<span>", {
+    //   class: "title"
+    // }).text(title));
+    // stu_project.append(pro_title);
     //project images
     proj_images = $("<div>", {
       class: "images", id: '' + stu_id,
@@ -78,9 +107,12 @@ $(document).ready(function () {
     proj_images.append($("<img>", {
       src: images[0],
       id: `proj_img_${stu_id}_0`,
+      alt: `${title} image`
     }));
     stu_project.append(proj_images);
 
+
+    console.log('-----------------')
     //next and prev button for scrolling through project images 
     var btns = 2; // 0 = previous, 1 = next
     var value = 'previous';
@@ -98,32 +130,56 @@ $(document).ready(function () {
     stu_project.append(buttons)
 
     //project description
-    stu_project.append($("<p>", {
+    proj_details = $('<div>', {
+      class: 'project_details'
+    });
+    proj_details.append($("<p>", {
       class: "desc"
     }).text(desc));
 
+    // proj_details.append(keys);
+    wrapper.append(proj_details)
+
+    //Project title
+
+    pro_title = $("<div>", {
+      class: 'title'
+    }).append($("<span>", {
+      class: "title"
+    }).text(title));
+
+    stu_resources = $('<div>', { class: 'keys' });
     //keywords
     keys = $('<div>', {
       class: 'keywords'
-    }).text(keywords);
-    stu_project.append(keys);
+    })
+    for (i in keywords) {
+      console.log('test me ' + keywords[i])
+      keys.append(`<span class='chip'>${keywords[i]}</span>`);
+    }
 
-    //vide, presentation, brochure, resume  => student resources
-    stu_resources = $('<div>', {
+    // console.log(keywords)
+
+    stu_resources.append(pro_title);
+    stu_resources.append(keys);
+
+    //vide, presentation, brochure, resume  => student resources, all resources combined to => reso
+
+    reso = $('<div>', {
       class: 'resources',
-    }).append(`<span><a href=${presentation}>Presentation</a></span>`)
-      .append(`<span><a href=${brochure}>Brochure</a></span>`)
-      .append(`<span><a href=${resume}>Resume</a></span>`)
-      .append(`<span><a href=${video}>Video</a></span>`);
+    }).append(`<span class='circle' style="background-color:${presentation.color}"><a href=${presentation.link}>Presentation</a></span>`)
+      .append(`<span class='circle' style="background-color:${brochure.color}"><a href=${brochure.link}>Brochure</a></span>`)
+      .append(`<span class='circle' style="background-color:${resume.color}"><a href=${resume.link}>Resume</a></span>`)
+      .append(`<span class='circle' style="background-color:${video.color}"><a href=${video.link}>Video</a></span>`);
 
-
-    stu_project.append(stu_resources);
+    proj_details.append(reso);
 
     wrapper.append(stu_project);
+    wrapper.append(stu_resources);
+
 
     $(".container").append(wrapper);
   }
-
 
   /*  --------------------------------------- next and previous button function --------------------------------*/
   //when previous button is clicked 
@@ -166,11 +222,104 @@ function nextORprev(button, id) {
   }
   var new_id = img_id.substring(0, img_id.lastIndexOf('_'));
 
-  //transition images function below
-  $(`#${img_id}`).fadeOut(1000, function () {
-    console.log('gets here')
-    $(`#${img_id}`).attr('src', '');
-    $(`#${img_id}`).attr('src', `${images[current_index]}`).fadeIn(1000);
-    $(`#${img_id}`).attr('id', `${new_id}_${current_index}`);
+  if (images[current_index] != "") {
+    console.log('this is valid ' + images[current_index])
+    $(`#${img_id}`).attr('src', `${images[current_index]}`);
+  } else {
+    console.log('image not found ' + images[current_index]);
+    $(`#${img_id}`).attr('src', `${image_not_found}`);
+  }
+  $(`#${img_id}`).attr('id', `${new_id}_${current_index}`);
+
+
+  // imageExists(images[parseInt(current_index)], function (exists) {
+  //   if (exists == true) {
+  //     console.log('checked true');
+  //     $(`#${img_id}`).attr('src', `${images[current_index]}`);
+  //   } else {
+  //     console.log('checked false');
+  //     $(`#${img_id}`).attr('src', `${image_not_found}`);
+  //   }
+
+  //   $(`#${img_id}`).attr('id', `${new_id}_${current_index}`);
+
+  // });
+  // $(`#${img_id}`).fadeOut(1000, function () {
+  //   console.log('gets here')
+  //   $(`#${img_id}`).attr('src', '');
+  //   $(`#${img_id}`).attr('src', `${images[current_index]}`).fadeIn(1000);
+  //   $(`#${img_id}`).attr('id', `${new_id}_${current_index}`);
+  // });
+}
+
+
+/*---------------------------------------Checking if image url is valid and exists -------------------------------------*/
+/*
+                                                NOTE TO SELF
+
+Image checking doesn't work yet. The problem seems to be with the 'foreach' loop. foreach loop and async function
+don't work well together. I need the images links validated before the rest of the respective part of page loads and
+*the problem I am having is that the loops would complete first before all links are validated.
+I am adding minor checks for images, I will come back to this later.
+
+*/
+/*
+
+
+function imageExists(url, callback) {
+  console.log('this is the url it is getting = ' + url);
+  var img = new Image();
+  img.onload = function () { callback(true); };
+  img.onerror = function () { callback(false); };
+  img.src = url;
+}
+
+async function imageExist(url, callback) {
+  return new Promise(resolve => {
+    var img = new Image();
+    img.onload = function () { callback(true); };
+    img.onerror = function () { callback(false); };
+    img.src = url;
+  })
+}
+
+function checkImage(url) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+    img.src = url;
+  });
+};
+
+
+function asyncTest() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('I am tired');
+    }, 5000);
   });
 }
+
+
+$(`#${img_id}`).fadeOut(1000, function () {
+    // console.log('CHECKING FOR OLD ')
+    //checking if image exists
+    imageExists(images[parseInt(current_index)], function (exists) {
+      console.log('studet id ' + new_id + 'current index' + current_index)
+      console.log('RESULT: url=' + images[current_index] + ', exists=' + exists);
+      $(`#${img_id}`).attr('src', '');
+
+      if (exists == true) {
+        console.log('checked true');
+        $(`#${img_id}`).attr('src', `${images[current_index]}`).show();
+      } else {
+        console.log('checked false');
+        $(`#${img_id}`).attr('src', `${image_not_found}`).show();
+      }
+    })
+
+    $(`#${img_id}`).attr('id', `${new_id}_${current_index}`);
+  });
+
+*/
